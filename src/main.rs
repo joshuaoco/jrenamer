d3a88829fff1e3e06ad3062b3d36d30c9211f084
Joshua O'Connor <joshua@joshuao.com>
2021-01-16 09:29:26 +0000

    More stuff

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,96 @@
-use anyhow::{bail, Context, Result};
-use clap::{App, Arg};
-use std::{
-    collections::HashMap,
-    ffi::OsStr,
-    fs::Metadata,
-    path::{Path, PathBuf},
-    time::SystemTime,
-};
-
+use anyhow::{anyhow, bail, Context, Error, Result};
+use clap::{App, Arg, ArgMatches};
+use file_info::FileInfo;
+use std::{collections::HashMap, ffi::OsStr, io::{self, Write}, path::Path, process::{Command, Stdio}};
+#[macro_use] extern crate lazy_static;
 mod file_info;
+mod file;
+use file::File;
 
 
-
-
-// The pair of variable name (as its used in rename string) and its value as a string
-struct Fragments(HashMap<String, String>);
-
-// This is the struct which stores all the files as they're passed from the commandline
-// It can contain files which don't exist (represented by Err in file_info), and thus is to be used for reporting errors per file
-struct Files<'a> {
-    path_provided: &'a OsStr,
-    file_info: Result<file_info::FileInfo<'a>>,
-    fragments: Fragments
-
+fn path_exists<'a, T: AsRef<Path> + ?Sized>(path: &'a T) -> Result<&'a Path> {
+    let path = path.as_ref();
+    if path.exists() {
+        Ok(path)
+    } else {
+        Err(anyhow!("File {} doesn't exist", path.to_string_lossy()))
+    }
 }
 
+
 fn main() -> Result<()> {
-    let matches = App::new("JRenamer")
+    let matches = get_matches();
+
+    // First we resolve the paths into their file info format
+    let mut files = get_files(&matches)?;
+
+    // Then we resolve scripts, checking if they exist and reporting if they don't
+    let scripts = get_scripts(&matches);
+
+    // Run the scripts against the files, building up fragments as we go
+    for f in files.iter_mut()  {
+        if f.exists(){
+
+            f.add_file_info_to_fragments();
+
+            for s in scripts.iter() {
+                f.run_script(s)?;
+            }
+
+            println!("File: {}", f.path_provided.to_string_lossy());
+            println!("Fragments from scripts available: {:?}",  f.fragments);
+
+        //TODO Get user input for filename
+            let mut input = String::new();
+            match io::stdin().read_line(&mut input) {
+                Ok(n) => {
+                    println!("{} bytes read", n);
+                    println!("{}", input);
+                }
+                Err(error) => println!("error: {}", error),
+            }
+
+        //TODO Parse user input and insert fragments where needed
+            println!("{}", f.parse_fstring(&input));
+        }
+
+    }
+    Ok(())
+}
+
+
+
+
+// TODO: Genericise
+
+fn get_files<'a>(ms: &'a ArgMatches) -> Result<Vec<File<'a>>> {
+    // First we resolve the paths into their file info format
+    match ms.values_of("input") {
+        Some(vals) => Ok(vals.map(File::from_path).collect::<Vec<File>>()),
+        None => {
+            bail!("You must supply at least one input item")
+        }
+    }
+}
+
+fn get_scripts<'a>(ms: &'a ArgMatches) -> Vec<&'a Path> {
+    match ms.values_of("script") {
+        Some(vals) => vals
+            .filter_map(|v| match path_exists(v) {
+                Ok(p) => Some(p),
+                Err(e) => {
+                    eprintln!("{}", e);
+                    None
+                }
+            })
+            .collect::<Vec<&Path>>(),
+
+        None => Vec::new(),
+    }
+}
+
+fn get_matches() -> ArgMatches<'static> {
+    App::new("JRenamer")
         .version("unversioned")
         .author("Joshua O. <joshua@joshuao.com>")
         .about("Renames files scriptingly")
@@ -35,27 +99,14 @@ fn main() -> Result<()> {
                 .help("Names of the files to rename")
                 .min_values(1),
         )
-        .get_matches();
-
-    let fragments = Fragments(HashMap::new());
-
-    let finfos = match matches.values_of("input") {
-        Some(vals) => vals.filter_map(|v| file_info::FileInfo::from_path(v).ok()),
-        None => {
-            bail!("You must supply at least one input item")
-        }
-    };
-
-
-
-
-    //TODO: Make file info format neater, decide on OsStr
-    for f in finfos.filter_map(|fi| serde_json::to_string(&fi).ok()) {
-        println!("{}", f);
-    }
-
-
-
-
-    Ok(())
+        .arg(
+            Arg::with_name("script")
+                .short("s")
+                .long("script")
+                .value_name("script")
+                .multiple(true)
+                .help("Sets a custom config file")
+                .takes_value(true),
+        )
+        .get_matches()
 }
